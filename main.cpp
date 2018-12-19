65e56a5684bdacc0dffe6f08e5ed55865b88d4e2
guanyu4312 <784558153@qq.com>
2018-12-19 16:57:04 +0800

    旋转数字

--- main.cpp
+++ main.cpp
@@ -11,12 +11,15 @@ int main() {
 //    for (int i = 0; i < 10; ++i) {
 //        cout<<a[i]<<" ";
 //    }
-    int age[10] = {6,7,9,11,15,32,65,45,0,12};
-    sortAges(age, 10);
-
-    for (int i = 0; i < 10; ++i) {
-        cout<<age[i]<<" ";
-    }
+//    int age[10] = {6,7,9,11,15,32,65,45,0,12};
+    int numbers[5] = {1,0,1,1,1};
+    int numbers2[5] = {1,1,1,0,1};
+//    int numbers[10] = {6,7,8,9,1,2,3,4,5,6};
+//    sortAges(age, 10);
+    gh::print(Min(numbers,5));
+//    for (int i = 0; i < 10; ++i) {
+//        cout<<age[i]<<" ";
+//    }
     return 0;
 }
 
