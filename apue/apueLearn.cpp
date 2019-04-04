42d71c49f4b0a71e74cd2316c5ba00554dd256ab
关亨 <guan.heng@bendis.cc>
2019-04-04 13:37:15 +0800

    apue

--- apue/apueLearn.cpp
+++ apue/apueLearn.cpp
@@ -20,4 +20,48 @@ namespace apue {
         closedir(dp);
         exit(0);
     }
+
+    void charatatime(char *str) {
+        char *ptr;
+        int c;
+        setbuf(stdout, NULL);
+        for (ptr = str; (c = *ptr++)!=0;)
+            putc(c, stdout);
+    }
+
+    void testFork() {
+        pid_t  pid;
+        TELL_WAIT();
+        if ((pid = fork()) < 0) {
+            err_sys("fork error");
+        } else if(pid == 0) {
+            WAIT_PARENT();
+            apue::charatatime("output from child\n");
+        } else {
+            apue::charatatime("output from parent\n");
+            TELL_CHILD(pid);
+        }
+        exit(0);
+    }
 }
+
+//int globvar = 6;
+//char buf[] = "a write to stdout\n";
+//int main(int argc, char* argv[]) {
+//    int var;
+//    pid_t pid;
+//    var = 88;
+//    if (write(STDOUT_FILENO, buf, sizeof(buf) - 1) != sizeof(buf) -1)
+//        err_sys("write error");
+//    printf("before fork\n");
+//    if ((pid = fork()) < 0) {
+//        err_sys("fork error");
+//    } else if (pid == 0) {
+//        globvar++;
+//        var++;
+//    } else {
+//        sleep(2);
+//    }
+//    printf("pid = %ld, glob = %d, var = %d\n", (long)getpid(), globvar, var);
+//    exit(0);
+//}
--- main.cpp
+++ main.cpp
@@ -1,20 +1,41 @@
 #include <iostream>
 #include <algorithm>
 #include <dirent.h>
+#include <sys/wait.h>
 #include <set>
 #include "apueLearn.h"
 #include "unpLearn.h"
 #include "ghlib/gh.h"
 #include "SwordToOffer/Interview.h"
+#include "apue.h"
 using namespace std;
 using namespace swordToOffer;
-
+char *env_init[] = {"USER=gh", "PATH=/home/gh/Desktop/gh_code/GHCXX/cmake-build-debug/", NULL};
 int main(int argc, char* argv[]) {
-    int array[8] = {1,-2,3,10,-4,7,2,-5};
-    gh::print(FindGreatestSumOfSubArray(array, 8));
-
-
-
+//    pid_t pid;
+//    if ((pid = fork()) < 0) {
+//        err_sys("fork error");
+//    } else if (pid == 0) {
+//        if (execle("/home/gh/Desktop/gh_code/GHCXX/cmake-build-debug/echoall", "echoall", "myarg1", "MY ARG2", (char*)0, env_init) < 0) {
+//            err_sys("execle error");
+//        }
+//    }
+//
+//    if (waitpid(pid, NULL, 0) < 0)
+//        err_sys("wait error");
+//    if ((pid = fork()) < 0) {
+//        err_sys("fork error");
+//    } else if (pid == 0) {
+//        if (execlp("echoall", "echoall", "only 1 arg", (char*)0) < 0)
+//            err_sys("execlp error");
+//    }
+//    exit(0);
+    char *p;
+    p = getlogin();
+    if (p == nullptr) {
+        err_sys("getlogin error");
+    }
+    gh::print(p);
 
 }
 
